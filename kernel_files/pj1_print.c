bddd4c8abd4f2dfe9d04f0f81602b37b5513152f
gracesilia <45330037+gracesilia@users.noreply.github.com>
2020-04-29 19:12:30 +0800

    add kernel files

---
+++ kernel_files/pj1_print.c
@@ -0,0 +1,8 @@
+// syscall #333
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+
+asmlinkage void sys_pj1_print(int pid, long start_time, long end_time) {
+    static const long BASE = 1000000000;
+    printk(KERN_INFO "[Project1] %d %ld.%09ld %ld.%09ld\n", pid, start_time / BASE, start_time % BASE, end_time / BASE, end_time % BASE);
+}
